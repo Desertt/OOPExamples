e840fa2404885c15810de6c43f1f49f6b35d878f
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-01-29 16:58:11 +0300

    Add project files.

---
+++ Sum/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Sum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.textBoxFirstNumber = new System.Windows.Forms.TextBox();
+            this.textBoxSecondNumber = new System.Windows.Forms.TextBox();
+            this.labelFirstNumber = new System.Windows.Forms.Label();
+            this.labelSecondNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonSum.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSum.Location = new System.Drawing.Point(312, 243);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(153, 38);
+            this.buttonSum.TabIndex = 0;
+            this.buttonSum.Text = "Sum";
+            this.buttonSum.UseVisualStyleBackColor = false;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // textBoxFirstNumber
+            // 
+            this.textBoxFirstNumber.Location = new System.Drawing.Point(312, 161);
+            this.textBoxFirstNumber.Name = "textBoxFirstNumber";
+            this.textBoxFirstNumber.Size = new System.Drawing.Size(153, 22);
+            this.textBoxFirstNumber.TabIndex = 1;
+            // 
+            // textBoxSecondNumber
+            // 
+            this.textBoxSecondNumber.Location = new System.Drawing.Point(312, 189);
+            this.textBoxSecondNumber.Name = "textBoxSecondNumber";
+            this.textBoxSecondNumber.Size = new System.Drawing.Size(153, 22);
+            this.textBoxSecondNumber.TabIndex = 2;
+            // 
+            // labelFirstNumber
+            // 
+            this.labelFirstNumber.AutoSize = true;
+            this.labelFirstNumber.Location = new System.Drawing.Point(221, 166);
+            this.labelFirstNumber.Name = "labelFirstNumber";
+            this.labelFirstNumber.Size = new System.Drawing.Size(85, 17);
+            this.labelFirstNumber.TabIndex = 3;
+            this.labelFirstNumber.Text = "Birinci Sayı :";
+            // 
+            // labelSecondNumber
+            // 
+            this.labelSecondNumber.AutoSize = true;
+            this.labelSecondNumber.Location = new System.Drawing.Point(221, 194);
+            this.labelSecondNumber.Name = "labelSecondNumber";
+            this.labelSecondNumber.Size = new System.Drawing.Size(78, 17);
+            this.labelSecondNumber.TabIndex = 4;
+            this.labelSecondNumber.Text = "İkinci Sayı :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelSecondNumber);
+            this.Controls.Add(this.labelFirstNumber);
+            this.Controls.Add(this.textBoxSecondNumber);
+            this.Controls.Add(this.textBoxFirstNumber);
+            this.Controls.Add(this.buttonSum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSum;
+        private System.Windows.Forms.TextBox textBoxFirstNumber;
+        private System.Windows.Forms.TextBox textBoxSecondNumber;
+        private System.Windows.Forms.Label labelFirstNumber;
+        private System.Windows.Forms.Label labelSecondNumber;
+    }
+}
+
---
+++ Sum/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            int birinciSayi, ikinciSayi, toplam;
+            birinciSayi = Convert.ToInt32(textBoxFirstNumber.Text);
+            ikinciSayi = Convert.ToInt32(textBoxSecondNumber.Text);
+            toplam = birinciSayi + ikinciSayi;
+            MessageBox.Show("Girilen Sayıların Toplamı : \n" + toplam);
+        }
+    }
+}
