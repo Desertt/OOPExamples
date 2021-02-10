ba46aeb66afc7a7dcedb92337cfd85f447dea826
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-02-10 14:39:32 +0300

    Topla,Doğrula

--- Sum/Form1.cs
+++ Sum/Form1.cs
@@ -17,13 +17,45 @@ namespace Sum
             InitializeComponent();
         }
 
+        private int Topla(int sayi1 ,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        private Boolean Dogrula() {
+
+            try
+            {
+                Convert.ToInt32(textBoxFirstNumber.Text);
+                Convert.ToInt32(textBoxSecondNumber.Text);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
         private void buttonSum_Click(object sender, EventArgs e)
         {
-            int birinciSayi, ikinciSayi, toplam;
-            birinciSayi = Convert.ToInt32(textBoxFirstNumber.Text);
-            ikinciSayi = Convert.ToInt32(textBoxSecondNumber.Text);
-            toplam = birinciSayi + ikinciSayi;
-            MessageBox.Show("Girilen Sayıların Toplamı : \n" + toplam);
+
+            if (Dogrula())
+            {
+                int toplam = Topla(Convert.ToInt32(textBoxFirstNumber.Text), Convert.ToInt32(textBoxSecondNumber.Text));
+                MessageBox.Show("Toplam : " + toplam);
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz Değerlerde Hata Var !");
+            }
+            //int birinciSayi, ikinciSayi, toplam;
+            //birinciSayi = Convert.ToInt32(textBoxFirstNumber.Text);
+            //ikinciSayi = Convert.ToInt32(textBoxSecondNumber.Text);
+            //toplam = birinciSayi + ikinciSayi;
+            //MessageBox.Show("Girilen Sayıların Toplamı : \n" + toplam);
         }
     }
 }
